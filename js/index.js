dcd7d145467131380faf93d4ffcc58929ead431f
raked <raked@users.noreply.github.com>
2021-07-11 18:22:21 -0400

    Added click factor

--- js/index.js
+++ js/index.js
@@ -1,8 +1,9 @@
 let dataBalance = 0;
 let networkConnections = 0;
+let clickFactor = 1;
 
 function mineData(number) {
-    dataBalance += number;
+    dataBalance += number * clickFactor;
     document.getElementById("dataBalance").innerHTML = dataBalance;
 }
 
@@ -12,6 +13,16 @@ function buyNetworkConnections() {
         networkConnections++;
         dataBalance -= networkConnectionsCost;
         document.getElementById("networkConnections").innerHTML = networkConnections;
+        document.getElementById("dataBalance").innerHTML = dataBalance;
+    }
+}
+
+function buyClickFactor(){
+    let clickFactorCost = 100 + Math.floor((Math.pow(1.1, clickFactor)));
+    if (dataBalance >= clickFactorCost){
+        clickFactor++;
+        dataBalance -= clickFactorCost;
+        document.getElementById("clickFactor").innerHTML = clickFactor;
         document.getElementById("dataBalance").innerHTML = dataBalance;
     }
 }
