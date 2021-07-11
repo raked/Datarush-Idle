a621d2e01c81f20d4baef0710132147fc0a80f45
raked <raked@users.noreply.github.com>
2021-07-11 12:02:13 -0400

    Basic clicker functioning now

--- js/index.js
+++ js/index.js
@@ -1,6 +1,26 @@
 let dataBalance = 0;
+let networkConnections = 0;
 
 function mineData(number) {
     dataBalance += number;
     document.getElementById("dataBalance").innerHTML = dataBalance;
 }
+
+function buyNetworkConnections() {
+    let networkConnectionsCost = 40;
+    if (dataBalance >= networkConnectionsCost) {
+        networkConnections++;
+        dataBalance -= networkConnectionsCost;
+        document.getElementById("networkConnections").innerHTML = networkConnections;
+        document.getElementById("dataBalance").innerHTML = dataBalance;
+    }
+}
+
+function networkConnectionsWork(){
+    if (networkConnections > 0){
+        dataBalance += 1 * networkConnections;
+        document.getElementById("dataBalance").innerHTML = dataBalance;
+    }
+}
+
+setInterval("networkConnectionsWork()", 1000)
