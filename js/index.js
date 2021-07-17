7149c309116613ef457c10378036152726c3a83c
raked <raked@users.noreply.github.com>
2021-07-17 18:50:15 -0400

    Added spider mechanic

--- js/index.js
+++ js/index.js
@@ -2,6 +2,7 @@ let dataBalance = 0;
 let networkConnections = 0;
 let clickFactor = 1;
 let clickCombo = 1;
+let enemyCounter = 0;
 
 function mineData(number) {
     clickCombo += .5;
@@ -20,7 +21,7 @@ function clickComboDecay(){
     }
 }
 
-setInterval("clickComboDecay()", 1000)
+setInterval("clickComboDecay()", 1000);
 
 function buyNetworkConnections() {
     let networkConnectionsCost = 40 + Math.floor((Math.pow(1.1, networkConnections)));
@@ -49,4 +50,37 @@ function networkConnectionsWork(){
     }
 }
 
-setInterval("networkConnectionsWork()", 1000)
+
+function randomReward(){
+    let bodyDiv = document.body;
+    let newDivName = "randomReward" + enemyCounter;
+    let parentDiv = document.createElement("div");
+    parentDiv.setAttribute("id", newDivName);
+    parentDiv.setAttribute("class", "randomReward");
+    let winWidth = window.innerWidth;
+    let winHeight = window.innerHeight;
+    let imgElem = document.createElement("img");
+    bodyDiv.appendChild(parentDiv);
+    imgElem.src = "assets/images/spider_from_datarush.png";
+    determiningNumber = Math.random();
+    if(determiningNumber < 0.001){
+        parentDiv.appendChild(imgElem);
+
+        let randomTop = getRandomNumber(0, winHeight - 125);
+        let randomLeft = getRandomNumber(0, winWidth - 125);
+
+        parentDiv.style.top = randomTop + "px";
+        parentDiv.style.left = randomLeft + "px";
+
+        enemyCounter++;
+    }
+}
+
+// function that returns a random number between a min and max
+function getRandomNumber(min, max) {
+    return Math.random() * (max - min) + min;
+}
+
+setInterval("randomReward()", 1000);
+
+setInterval("networkConnectionsWork()", 1000);
